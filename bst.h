88ec68d999c74976954110e664623e424dd3fa03
taro <higepon@gmail.com>
2011-10-05 21:48:01 +0900

    Initial import.

---
+++ bst.h
@@ -0,0 +1,155 @@
+/*
+ *   Copyright (c) 2011 Higepon(Taro Minowa) <higepon@users.sourceforge.jp>
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#ifndef _USERS_TARO_BINARY_SEARCH_TREE_BST_H_
+#define _USERS_TARO_BINARY_SEARCH_TREE_BST_H_
+
+#include <stdlib.h>
+
+template <class T, class S> class BinarySearchTree {
+ public:
+  BinarySearchTree() : root_(NULL) {
+  }
+
+  virtual ~BinarySearchTree() {
+  }
+
+  void Add(const T key, S value) {
+    if (root_ == NULL) {
+      root_ = new Node(key, value);
+      return;
+    }
+    // N.B.
+    // Since this is *NOT* a balanced tree,
+    // we use iterative approach instread of recursive one
+    // to avoid stack overflow.
+    for (Node* current = root_;;) {
+      if (current->key == key) {
+        current->value = value;
+        return;
+      } else if (current->key < key) {
+        if (current->right == NULL) {
+          current->right = new Node(key, value);
+          return;
+        } else {
+          current = current->right;
+        }
+      } else {
+        if (current->left == NULL) {
+          current->left = new Node(key, value);
+          return;
+        } else {
+          current = current->left;
+        }
+      }
+    }
+  }
+
+  S Get(const T key, bool* found = NULL) const {
+    for (Node* current = root_;;) {
+      if (current == NULL) {
+        if (found) {
+          *found = false;
+        }
+        return S();
+      } else if (current->key == key) {
+        if (found) {
+          *found = true;
+        }
+        return current->value;
+      } else if (current->key < key) {
+          current = current->right;
+      } else {
+          current = current->left;
+      }
+    }
+  }
+
+  void Remove(const T key, bool* removed = NULL) {
+    Node* parent = NULL;
+    for (Node* current = root_;;) {
+      if (current == NULL) {
+        if (removed) {
+          *removed = false;
+        }
+        return;
+      } else if (current->key == key) {
+        RemoveNode(parent, current);
+        if (removed) {
+          *removed = true;
+        }
+        return;
+      } else if (current->key < key) {
+        parent = current;
+        current = current->right;
+      } else {
+        parent = current;
+        current = current->left;
+      }
+    }
+  }
+
+ private:
+  struct Node {
+    Node(const T key, S value, Node* left = NULL, Node* right = NULL) :
+        key(key),
+        value(value),
+        left(left),
+        right(right) {}
+    bool IsLeaf() const {
+      return left == NULL && right == NULL;
+    }
+    T key;
+    S value;
+    Node* left;
+    Node* right;
+  };
+
+  bool hasOneChild(Node* node) const {
+    return (node->left == NULL && node->right != NULL)
+        || (node->left != NULL && node->right == NULL);
+  }
+
+  void RemoveNode(Node* parent, Node* node) {
+    if (node->IsLeaf()) {
+      if (parent == NULL) {
+        root_ = NULL;
+      } else if (parent->left == node) {
+        parent->left = NULL;
+      } else {
+        parent->right = NULL;
+      }
+      delete node;
+    } else if (hasOneChild(node)) {
+
+    }
+  }
+
+  Node* root_;
+};
+
+#endif  // _USERS_TARO_BINARY_SEARCH_TREE_BST_H_
---
+++ test.cpp
@@ -0,0 +1,189 @@
+/*
+ *   Copyright (c) 2011  Higepon(Taro Minowa)  <higepon@users.sourceforge.jp>
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#include <stdint.h>
+#include <gtest/gtest.h>
+#include <algorithm>
+#include "./bst.h"
+
+namespace {
+
+class BinarySearchTreeTest : public ::testing::Test {
+ protected:
+  BinarySearchTree<int, int> bst_;
+ public:
+  BinarySearchTreeTest()  {
+  }
+
+  virtual ~BinarySearchTreeTest() {
+  }
+};
+
+TEST_F(BinarySearchTreeTest, NotFoundWhenEmpty) {
+  bool found = true;
+  EXPECT_EQ(0, bst_.Get(1, &found));
+  EXPECT_FALSE(found);
+}
+
+TEST_F(BinarySearchTreeTest, CanAddAndGet) {
+  bool found = false;
+  bst_.Add(1, 2);
+  EXPECT_EQ(2, bst_.Get(1, &found));
+  EXPECT_TRUE(found);
+}
+
+TEST_F(BinarySearchTreeTest, AddTwoValuesGetBoth) {
+  // 1
+  //   3
+  bst_.Add(1, 2);
+  bst_.Add(3, 4);
+  bool found = false;
+  EXPECT_EQ(2, bst_.Get(1, &found));
+  EXPECT_TRUE(found);
+
+  bool found2 = false;
+  EXPECT_EQ(4, bst_.Get(3, &found2));
+  EXPECT_TRUE(found2);
+}
+
+TEST_F(BinarySearchTreeTest, AddTwoValuesGetBoth2) {
+  //   3
+  // 1
+  bst_.Add(3, 4);
+  bst_.Add(1, 2);
+  bool found = false;
+  EXPECT_EQ(2, bst_.Get(1, &found));
+  EXPECT_TRUE(found);
+
+  bool found2 = false;
+  EXPECT_EQ(4, bst_.Get(3, &found2));
+  EXPECT_TRUE(found2);
+}
+
+TEST_F(BinarySearchTreeTest, OverWriteValue) {
+  bst_.Add(3, 4);
+  EXPECT_EQ(4, bst_.Get(3));
+  bst_.Add(3, 5);
+  EXPECT_EQ(5, bst_.Get(3));
+}
+
+TEST_F(BinarySearchTreeTest, ComplexTreeAndGet) {
+  //      8
+  //   5     10
+  // 4   7 9    11
+  bst_.Add(8, 8);
+  bst_.Add(10, 10);
+  bst_.Add(5, 5);
+  bst_.Add(11, 11);
+  bst_.Add(9, 9);
+  bst_.Add(4, 4);
+  bst_.Add(7, 7);
+
+  EXPECT_EQ(4, bst_.Get(4));
+  EXPECT_EQ(5, bst_.Get(5));
+  EXPECT_EQ(7, bst_.Get(7));
+  EXPECT_EQ(8, bst_.Get(8));
+  EXPECT_EQ(9, bst_.Get(9));
+  EXPECT_EQ(10, bst_.Get(10));
+  EXPECT_EQ(11, bst_.Get(11));
+}
+
+TEST_F(BinarySearchTreeTest, RemoveFailWhenEmpty) {
+  bool removed = true;
+  bst_.Remove(8, &removed);
+  EXPECT_FALSE(removed);
+}
+
+TEST_F(BinarySearchTreeTest, RemoveOnlyOneRoot) {
+  // 8
+  bst_.Add(8, 8);
+  bool removed = false;
+  bst_.Remove(8, &removed);
+
+  bool found = true;
+  EXPECT_EQ(0, bst_.Get(8, &found));
+  EXPECT_FALSE(found);
+  EXPECT_TRUE(removed);
+}
+
+TEST_F(BinarySearchTreeTest, RemoveLeaf) {
+  //   8
+  // 5   10
+  bst_.Add(8, 8);
+  bst_.Add(10, 10);
+  bst_.Add(5, 5);
+  bool removed = false;
+  bst_.Remove(5, &removed);
+  EXPECT_TRUE(removed);
+
+  bool found = true;
+  EXPECT_EQ(0, bst_.Get(5, &found));
+  EXPECT_FALSE(found);
+}
+
+TEST_F(BinarySearchTreeTest, RemoveLeaf2) {
+  //   8
+  // 5   10
+  bst_.Add(8, 8);
+  bst_.Add(10, 10);
+  bst_.Add(5, 5);
+  bool removed = false;
+  bst_.Remove(10, &removed);
+  EXPECT_TRUE(removed);
+
+  bool found = true;
+  EXPECT_EQ(0, bst_.Get(10, &found));
+  EXPECT_FALSE(found);
+}
+
+TEST_F(BinarySearchTreeTest, RemoveNodeWithOneChild) {
+  //   8
+  //     10
+  //        11
+  bst_.Add(8, 8);
+  bst_.Add(10, 10);
+  bst_.Add(11, 11);
+  bool removed = false;
+  bst_.Remove(10, &removed);
+  EXPECT_TRUE(removed);
+
+  bool found = true;
+  EXPECT_EQ(0, bst_.Get(10, &found));
+  EXPECT_FALSE(found);
+
+  bool found2 = false;
+  EXPECT_EQ(0, bst_.Get(11, &found));
+  EXPECT_TRUE(found2);
+}
+
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
